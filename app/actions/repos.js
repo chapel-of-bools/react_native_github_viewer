936fea268ddaccb416a9e7910179abd3cca89908
Matthew Scott <matthewruelscott@gmail.com>
2016-09-26 16:56:10 -0400

    dispatch working

--- app/actions/repos.js
+++ app/actions/repos.js
@@ -7,12 +7,20 @@ export function fetchRepos(username) {
       `${encodeURIComponent(username)}`,
     ]
     return Api.get(`/users/${params}/repos`).then(resp => {
-      console.log(resp);
+      dispatch(setSearchedRepos({ repos: resp }));
     }).catch( (ex) => {
       console.log(ex);
     })
   }
 }
+
+export function setSearchedRepos( { repos } ) {
+return {
+  type: types.SET_SEARCHED_REPOS,
+  repos
+  }
+}
+
 export function addRepo() {
   return {
     type: types.ADD_REPO,

b497d5d49814c437c1a1acc029a1279a711296c8
Matthew Scott <matthewruelscott@gmail.com>
2016-09-26 17:02:21 -0400

    repo counter working

--- app/reducers/repos.js
+++ app/reducers/repos.js
@@ -2,11 +2,20 @@ import createReducer from '../lib/createReducer'
 import * as types from '../actions/types'
 
 export const searchedRepos = createReducer({}, {
-
+  [types.SET_SEARCHED_REPOS](state, action) {
+    let newState = {};
+    action.repos.forEach( (repo) => {
+      newState[repo.id] = repo
+    });
+    return newState;
+  }
 });
 
 export const repoCount = createReducer(0, {
+  [types.SET_SEARCHED_REPOS](state, action) {
+    return action.repos.length;
+  },
   [types.ADD_REPO](state, action) {
     return state + 1;
-  }
+  },
 })
